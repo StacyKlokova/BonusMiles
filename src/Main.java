31cff2583ef6d9ddc1e1ff7af181a0519ee7c557
StacyKlokova <nfirse@yandex.ru>
2023-01-22 19:39:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price;
+        int miles;
+
+        price = 20_000;
+        miles = 20;
+
+        int bonus = price / miles;
+
+        System.out.println(" Бонусные мили " + bonus);
+    }
+}
